751cc6f155138e8bea77e7d18851d41eabfd5e17
Slgaot <slobaton940@alumnes.politecnicllevant.cat>
2025-05-03 21:53:09 +0200

    Testing 7 part 4

--- JavaTesting/src/test/java/javaTesting/collectionsTesting/SimpleCollectionTest.java
+++ JavaTesting/src/test/java/javaTesting/collectionsTesting/SimpleCollectionTest.java
@@ -1,9 +1,8 @@
 package javaTesting.collectionsTesting;
 
 import org.junit.Test;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -38,6 +37,7 @@ public class SimpleCollectionTest {
         List<String> diesFeiners = new ArrayList<>();
         List<String> diesCapDeSetmana = new ArrayList<>();
         List<String> diesSenars = new ArrayList<>();
+        List<String> calculDies = new ArrayList<>();
 
         initDiesFeiners(diesFeiners);
 
@@ -73,6 +73,40 @@ public class SimpleCollectionTest {
 
         assertEquals(3, diesFeinersSenars.size());
         assertFalse(diesFeinersSenars.contains("dissabte"));
+
+        calculDies.addAll(diesSetmana);
+
+        assertTrue(calculDies.containsAll(diesFeiners));
+        assertTrue(calculDies.containsAll(diesCapDeSetmana));
+
+        calculDies.clear();
+        assertTrue(calculDies.isEmpty());
+
+        calculDies.addAll(diesSetmana);
+        calculDies.removeAll(diesCapDeSetmana);
+
+        assertEquals(5, calculDies.size());
+    }
+
+    @Test
+    public void provesAmbMapsTest() {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("fons", "negre");
+        map.put("menus", "blau");
+        map.put("dialeg", "verd");
+
+        assertEquals(3, map.size());
+        assertTrue(map.containsKey("menus"));
+        assertTrue(map.containsValue("negre"));
+
+        map.remove("dialeg");
+        assertEquals(2, map.size());
+
+        map.remove("clauInexistent");
+        assertEquals(2, map.size());
+
+        assertEquals("negre", map.get("fons"));
     }
 
     public void initDiesFeiners(List<String> dies) {
@@ -83,5 +117,15 @@ public class SimpleCollectionTest {
         dies.add("divendres");
     }
 
+    @Test
+    public void provesSetTest() {
+        Set<String> dies = new HashSet<>();
+
+        dies.add("dilluns");
+        dies.add("dilluns");
+        dies.add("dilluns");
+
+        assertEquals(dies.size(), 1);
+    }
 
 }
