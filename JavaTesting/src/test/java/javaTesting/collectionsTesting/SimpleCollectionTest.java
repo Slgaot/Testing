472b7dd09c9a467dff808c6b22160f8908a7532d
Slgaot <slobaton940@alumnes.politecnicllevant.cat>
2025-05-03 21:47:23 +0200

    Testing 7 part 3

--- JavaTesting/src/test/java/javaTesting/collectionsTesting/SimpleCollectionTest.java
+++ JavaTesting/src/test/java/javaTesting/collectionsTesting/SimpleCollectionTest.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertTrue;import static org.junit.Assert.assertTrue;
 
 public class SimpleCollectionTest {
 
@@ -28,4 +31,57 @@ public class SimpleCollectionTest {
 
         assertTrue(llistaNumeros.size() == 4);
     }
+
+    @Test
+    public void diesSetmana() {
+        List<String> diesSetmana = new ArrayList<>();
+        List<String> diesFeiners = new ArrayList<>();
+        List<String> diesCapDeSetmana = new ArrayList<>();
+        List<String> diesSenars = new ArrayList<>();
+
+        initDiesFeiners(diesFeiners);
+
+        assertTrue(diesFeiners.size() == 5);
+        assertEquals(0, diesSetmana.size());
+        assertFalse(diesSetmana.containsAll(diesFeiners));
+
+        diesSetmana.addAll(diesFeiners);
+
+        assertEquals(5, diesSetmana.size());
+        assertTrue(diesSetmana.containsAll(diesFeiners));
+
+        diesCapDeSetmana.add("dissabte");
+        diesCapDeSetmana.add("diumenge");
+
+        diesSetmana.addAll(diesCapDeSetmana);
+
+        assertEquals(7, diesSetmana.size());
+        assertTrue(diesSetmana.containsAll(diesCapDeSetmana));
+
+        diesSenars.addAll(diesSetmana);
+        assertTrue(diesSenars.contains("dimarts"));
+
+        diesSenars.remove(1);
+        assertFalse(diesSenars.contains("dimarts"));
+
+        diesSenars.remove("dijous");
+        assertFalse(diesSenars.contains("dijous"));
+
+        List<String> diesFeinersSenars = new ArrayList<>();
+        diesFeinersSenars.addAll(diesSenars);
+        diesFeinersSenars.removeAll(diesCapDeSetmana);
+
+        assertEquals(3, diesFeinersSenars.size());
+        assertFalse(diesFeinersSenars.contains("dissabte"));
+    }
+
+    public void initDiesFeiners(List<String> dies) {
+        dies.add("dilluns");
+        dies.add("dimarts");
+        dies.add("dimecres");
+        dies.add("dijous");
+        dies.add("divendres");
+    }
+
+
 }
